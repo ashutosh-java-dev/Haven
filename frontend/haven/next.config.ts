77b22425d2369001cfa1cbebc28236caf87ae6c4
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-04-01 15:03:35 +0530

    config or permitted free image APIs

--- frontend/haven/next.config.ts
+++ frontend/haven/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "**.pexels.com",
+    },
+    {
+      protocol: "https",
+      hostname: "**.unsplash.com",
+    },
+    {
+      protocol: "https",
+      hostname: "**.pixabay.com",
+    },
+  ],
+}
 };
 
 export default nextConfig;
