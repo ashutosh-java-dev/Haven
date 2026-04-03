38d3727f8d6df267efae4a98c11ed1f9a84617de
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2026-04-03 14:50:15 +0530

    created multiple components i.e. Header

--- frontend/haven/app/hotel/[hotelId]/page.tsx
+++ frontend/haven/app/hotel/[hotelId]/page.tsx
@@ -1,33 +1,15 @@
-import { getHotels } from "@/lib";
+import {getHotels} from "@/lib";
 import {Hotel} from "@/components/interface/hotel";
-import Images from "@/components/ui/Image";
+import Header from "@/components/feature/hotel/Header";
 
-export default async function HotelDetailsPage({params}: {params: Promise<{hotelId: string}>}) {
-    const {hotelId}: {hotelId: string} = await params;
+export default async function HotelDetailsPage({params}: { params: Promise<{ hotelId: string }> }) {
+    const {hotelId}: { hotelId: string } = await params;
     const hotel: Hotel = await getHotels(`${hotelId}`, "");
-    
+
     return (
-        <div style={{backgroundImage: `url(${hotel.media.coverImageUrl})`}} className={"w-screen h-screen bg-cover bg-no-repeat pt-36 text-white text-shadow-black text-shadow-md"}>
-            <div className={"flex items-center justify-center w-full"}>
-                <div className="container">
-                    <div>
-                        <h1 className={"text-4xl mb-3 font-bold"}>{hotel.name}</h1>
-                        <div className={" flex gap-4"}>
-                            <div className={"flex gap-3 text-yellow-300"}>
-                                {[...Array(hotel.starRating)].map((_, index) => {
-                                    return (
-                                        <span key={index} className={"size-7"}>{Images.STAR}</span>
-                                    );
-                                })}
-                            </div>
-                            <h3 className={"text-2xl"}>{hotel.brand}</h3>
-                        </div>
-                    </div>
-                    <div></div>
-                </div>
-
-            </div>
-
-        </div>
+        <>
+            <Header coverImageUrl={hotel.media.coverImageUrl} name={hotel.name} starRating={hotel.starRating} brand={hotel.brand}
+                    city={hotel.location.city} state={hotel.location.state} averageRating={hotel.ratings.averageRating} totalReviews={hotel.ratings.totalReviews} />
+        </>
     )
 }
---
+++ frontend/haven/components/feature/hotel/Header.tsx
@@ -0,0 +1,58 @@
+import Images from "@/components/ui/Image";
+
+interface PropsMap {
+    coverImageUrl: string;
+    name: string;
+    starRating: number;
+    brand: string;
+    city: string;
+    state: string;
+    averageRating: number;
+    totalReviews: number;
+}
+
+export default function Header(props: PropsMap){
+
+    return (
+        <div style={{backgroundImage: `url(${props.coverImageUrl})`}}
+             className={"w-screen h-scree bg-cover bg-no-repeat text-white bg-center"}>
+            <div className={"flex items-center justify-center pt-40 pb-60 bg-[#00000069]"}>
+                <div className="container flex justify-between">
+                    <div className={"flex flex-col gap-4"}>
+                        <h1 className={"text-4xl font-bold"}>{props.name}</h1>
+                        <div className={" flex gap-4"}>
+                            <div className={"flex gap-3 text-yellow-300"}>
+                                {[...Array(props.starRating)].map((_, index) => {
+                                    return (
+                                        <span key={index} className={"size-7"}>{Images.STAR}</span>
+                                    );
+                                })}
+                            </div>
+                            <h3 className={"text-2xl"}>{props.brand}</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <span className={"size-10"}>{Images.MAP_LOCATION}</span>
+                            <span>{props.city}, {props.state}</span>
+                        </div>
+                        <div
+                            className={"flex items-center gap-3 bg-teal-600 text-shadow-none rounded-lg py-2 px-3 w-fit"}>
+                            <span className={"size-5 text-white"}>{Images.STAR}</span>
+                            <span>{props.averageRating}</span>
+                            <span>({props.totalReviews})</span>
+                        </div>
+                    </div>
+                    <div>
+                        <button
+                            className="group relative inline-flex items-center overflow-hidden rounded-sm bg-teal-600 px-8 py-3 text-white cursor-pointer border border-teal-700">
+                            <span className="absolute -end-full size-5 transition-all group-hover:inset-e-4">
+                                {Images.CIRCLE_CARET_RIGHT}
+                            </span>
+                            <span
+                                className="text-sm font-medium transition-all group-hover:me-4"> Check Availability </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
